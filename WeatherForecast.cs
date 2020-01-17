e177831a2f8272aea0d0301be442efd81cf2d0cf
bahar <bahar@hemosoft.com>
2020-01-17 14:13:27 +0300

    yaml için deneme

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -6,7 +6,8 @@ namespace SampleProject
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get ; set; }
+        public int TemperatureC { get ; set; 
+        }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
